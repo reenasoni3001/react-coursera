71bd69e8047966264bd87fb9536359bd746e10bc
reenasoni3001 <reenaprahladsoni@gmail.com>
2019-09-23 19:43:29 +0530

    assignment 1

--- src/components/menucomponent.js
+++ src/components/menucomponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; //to create a react comp
 import {
   Card,
   CardImg,
@@ -7,33 +7,26 @@ import {
   CardBody,
   CardTitle
 } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Dishdetail from "./DishdetailComponent";
 
-class Menu extends Component {
+class Menu extends Component {        //this is how we add a new comp to our react app
   constructor(props) {
     super(props);
-    this.state = { selectedDish: null };
-  }
-  onDishSelect(dish) {
+    this.state = { 
+        selectedDish: null }    //state stores in the properties related to the component that can make use of it
+    }
+  
+  
+  onDishSelect=(dish)=> 
+  {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle> {dish.name} </CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
+    
+  
 
-  render() {
+  render() {               
     const menu = this.props.dishes.map(dish => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -47,11 +40,14 @@ class Menu extends Component {
         </div>
       );
     });
-    return (
-      <div className="container">
-        <div className="row">{menu}</div>
+    
+    return (                                             
+      <div className="container">                             
 
-        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+        <div className="row">{menu}</div>      
+        <div>
+        <Dishdetail dish={this.state.selectedDish} />
+        </div>
       </div>
     );
   }

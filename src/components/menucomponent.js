a033ab3f988904fd9ef39777c5cdba12ec51b25d
reenasoni3001 <reenaprahladsoni@gmail.com>
2019-09-24 17:09:58 +0530

    presentational and container component

--- src/components/menucomponent.js
+++ src/components/menucomponent.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react"; //to create a react comp
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle
-} from "reactstrap";
-import "bootstrap/dist/css/bootstrap.min.css"
-import Dishdetail from "./DishdetailComponent";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class Menu extends Component {        //this is how we add a new comp to our react app
+class Menu extends Component {
+  //this is how we add a new comp to our react app
   constructor(props) {
     super(props);
-    this.state = { 
-        selectedDish: null }    //state stores in the properties related to the component that can make use of it
-    }
-  
-  
-  onDishSelect=(dish)=> 
-  {
-    this.setState({ selectedDish: dish });
+    //state stores in the properties related to the component that can make use of it
   }
 
-    
-  
-
-  render() {               
+  render() {
     const menu = this.props.dishes.map(dish => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
+          <Card onClick={() => this.props.onClick(dish.id)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
 
             <CardImgOverlay>
@@ -40,14 +23,10 @@ class Menu extends Component {        //this is how we add a new comp to our rea
         </div>
       );
     });
-    
-    return (                                             
-      <div className="container">                             
 
-        <div className="row">{menu}</div>      
-        <div>
-        <Dishdetail dish={this.state.selectedDish} />
-        </div>
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
       </div>
     );
   }

b3b792f6e113ac55573eed8598240853806c219f
reenasoni3001 <reenaprahladsoni@gmail.com>
2019-09-24 18:45:52 +0530

    functional component

--- src/components/menucomponent.js
+++ src/components/menucomponent.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react"; //to create a react comp
+import React from "react"; //to create a react comp
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class Menu extends Component {
-  //this is how we add a new comp to our react app
-  constructor(props) {
-    super(props);
-    //state stores in the properties related to the component that can make use of it
-  }
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <Card onClick={() => onClick(dish.id)}>
+      <CardImg width="100%" src={dish.image} alt={dish.name} />
 
-  render() {
-    const menu = this.props.dishes.map(dish => {
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+      <CardImgOverlay>
+        <CardTitle> {dish.name} </CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+}
 
-            <CardImgOverlay>
-              <CardTitle> {dish.name} </CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
-
+const Menu = props => {
+  const menu = props.dishes.map(dish => {
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <div className="row">{menu}</div>
+    </div>
+  );
+};
 
 export default Menu;

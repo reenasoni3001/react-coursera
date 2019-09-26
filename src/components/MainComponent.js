5efb34ad0ecb3bf80666e98f5b5879328dee1144
reenasoni3001 <reenaprahladsoni@gmail.com>
2019-09-26 21:12:22 +0530

    Footer and CSS classes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Menu from "./menucomponent";
 import Dishdetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
+import Footer from './FooterComponent';
 import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
@@ -33,6 +34,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer />
       </div>
     );
   }
